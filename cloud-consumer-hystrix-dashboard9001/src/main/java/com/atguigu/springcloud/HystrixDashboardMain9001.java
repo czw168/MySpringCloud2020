54c3708241b099fdaa5076f9880087a4f12d53bc
czw <czw@qq.com>
2020-07-22 09:35:26 +0800

    第十章Hystrix断路器-控制台监控配置完成

--- cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
+++ cloud-consumer-hystrix-dashboard9001/src/main/java/com/atguigu/springcloud/HystrixDashboardMain9001.java
@@ -10,26 +10,12 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableHystrixDashboard
 public class HystrixDashboardMain9001 {
+
     public static void main(String[] args) {
         SpringApplication.run(HystrixDashboardMain9001.class);
     }
 
 
-    /**
-     * 此配置是为了服务监控而配置，与服务容错本身无观，springCloud 升级之后的坑
-     * ServletRegistrationBean因为springboot的默认路径不是/hystrix.stream
-     * 只要在自己的项目中配置上下面的servlet即可
-     * @return
-     */
-    @Bean
-    public ServletRegistrationBean getServlet(){
-        HystrixMetricsStreamServlet streamServlet = new HystrixMetricsStreamServlet();
-        ServletRegistrationBean<HystrixMetricsStreamServlet> registrationBean = new ServletRegistrationBean<>(streamServlet);
-        registrationBean.setLoadOnStartup(1);
-        registrationBean.addUrlMappings("/hystrix.stream");
-        registrationBean.setName("HystrixMetricsStreamServlet");
-        return registrationBean;
-    }
 
 
 }
