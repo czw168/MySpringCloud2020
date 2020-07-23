6f3254400f049ab3b2216c691b1b364c3e7fadde
czw <466282093@qq.com>
2020-07-23 20:46:06 +0800

    截止到第十五章SpringCloud Stream消息驱动-分组消费与持久化:完善了8803模块代码

---
+++ cloud-stream-rabbitmq-consumer8803/src/main/java/com/atguigu/springcloud/StreamRabbitMQMain8803.java
@@ -0,0 +1,12 @@
+package com.atguigu.springcloud;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamRabbitMQMain8803 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamRabbitMQMain8803.class, args);
+    }
+
+}
