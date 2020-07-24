d62be2421f111d2eedd390c76ae7a946b2ef92e7
czw <czw@qq.com>
2020-07-24 17:47:29 +0800

    截止第十八章SpringCloud Alibaba Nacos服务注册和配置中心 - Nacos集群和持久化配置(重要) - Linux版Nacos+MySQL生产环境配置

---
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/springcloud/controller/OrderNacosController.java
@@ -0,0 +1,26 @@
+package com.atguigu.springcloud.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+
+@RestController
+@Slf4j
+public class OrderNacosController {
+
+    @Resource
+    private RestTemplate restTemplate;
+
+    @Value("${service-url.nacos-user-service}")
+    private String serverUrl;
+
+    @GetMapping("/consumer/payment/nacos")
+    public String paymentInfo(){
+        return restTemplate.getForObject(serverUrl + "/payment/nacos/", String.class);
+    }
+
+}
